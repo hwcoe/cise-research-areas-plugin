66493cb8a4f34fade3c50b634c4b9a509148ba5d
Allison Logan <allisonlogan@Logan-Family-iMac.local>
2021-01-29 11:51:46 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Research Areas
+Contributors: Allison Logan
+This plugin is specifically for use with the CISE Child Theme template. It allows administrators to display research areas tiled format that also utilizes popups.
+
+## Description
+The CISE Research Areas plugin has been created specifically for use with the CISE Child Theme. This plugin allows admin to display posts using the Research Areas Single Page template under the category "research-areas-pg." 
+
+The specified templates, category and ACF Field Groups must be used for this plugin to work. 
+
+## Installation
+For help installing this (or any other) WordPress plugin, please read the [Managing Plugins](http://codex.wordpress.org/Managing_Plugins) article on the Codex.
+
+## Required Plugins
+* [Advanced Custom Fields](https://wordpress.org/plugins/advanced-custom-fields/)
+
+## Frequently Asked Questions
+Can I change the category that the plugin uses?
+No. This change has been made within the plugin and there are no settings options for this plugin.
+
+## Changelog
+
+v1.0 (2019-03-25) =
+* [NEW] Initial release
---
+++ research-areas.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Plugin Name: CISE Research Areas
+Description: Use this shortcode to display research areas under the "research-areas-pg" category<strong>[RESEARCH_AREAS type="post" posts_per_page="50" order="ASC" orderby="title" category_name="research-areas-pg"]</strong>
+Version: 1.1
+Author: Allison Logan
+Author URI: http://allisoncandreva.com/
+*/
+
+
+Class ResearchAreas {
+
+	public $plugin_dir;
+	public $plugin_url;
+
+	function  __construct(){
+		global $wpdb;
+		$prefix = $wpdb->prefix;
+		$this->plugin_dir = plugin_dir_path(__FILE__);
+		$this->plugin_url = plugin_dir_url(__FILE__);
+		add_shortcode( 'RESEARCH_AREAS', array($this, 'research_areas_shortcode' ) );
+		add_action( 'wp_enqueue_scripts', array($this,'researchareas_enqueue_scripts_styles' ));
+	}
+	
+	function researchareas_enqueue_scripts_styles(){
+		wp_enqueue_script('researchareas_fancybox_js', $this->plugin_url.'js/jquery.fancybox.min.js', array('jquery'), '1.0.0', true);
+		wp_enqueue_style('researchareas_fancybox_css', $this->plugin_url.'css/jquery.fancybox.min.css');
+		wp_enqueue_script('researchareas', $this->plugin_url.'js/researchareas.js', array('jquery'), '1.0.0', true);
+		wp_enqueue_style('researchareas', $this->plugin_url.'css/researchareas.css');
+	}
+	
+	public function research_areas_shortcode($atts) {
+
+		extract( shortcode_atts( array(
+			'posts_per_page' => '50',
+			'order' => 'ASC',
+			'orderby' => 'title',
+			'type'=>'type',	
+			'category_name' => 'research-areas-pg',
+		), $atts ) );
+		
+		$args = array(
+			'posts_per_page' => (int) $atts['posts_per_page'],
+			'post_type' =>$atts['type'],
+			'order' => $atts['order'],
+			'orderby' => $atts['orderby'],
+			'category_name' => $atts['category_name'],
+			'no_found_rows' => true,
+		);
+		
+		$dispCount  = (int) $posts_per_page;
+		if($dispCount==50){
+			$colmd = three;
+		}else if($dispCount=="3"){
+			$colmd = three; 
+		}else{
+			$colmd = three;
+		}
+		$query = new WP_Query( $args  );
+
+		$testimonials = '<div class="researchareas-tab">'; //col-md-12
+
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) : $query->the_post();
+				$post_id = get_the_ID();
+
+				$featimageURL = wp_get_attachment_url( get_post_thumbnail_id($post_id) );
+
+				$testimonials .= '<div class="researchbox" aria-label="' .get_the_title(). '">';
+										
+				$testimonials .= $this->wpse69204_excerpt(); 
+				$testimonials .= 
+					'<div class="fancyboxcont" id="post_'.$post_id.'">';
+				$testimonials .= 
+						'<div class="col-md-12 popupcont">';
+				$testimonials .= 
+							'<div class="popupcont-text"><h1>' .get_the_title(). '</h1>
+							<hr class="co-func-line">
+							<p>' .get_field('content_area'). '</p>' ;
+
+				$testimonials .= '</div></div></div></div>';
+			endwhile;
+			wp_reset_postdata();
+		} else { ?>
+			<p style="text-align:center;">There are no research areas to show. Please come back.</p>
+		<?php }
+		$testimonials .= '</div>';
+		return $testimonials;
+	} //end research_areas_shortcode function
+	
+	public function wpse69204_excerpt( $post_id = null )
+	{
+		global $post;
+		$research_post = $post_id ? get_post( $post_id ) : $post;
+		$featimageURL = wp_get_attachment_url( get_post_thumbnail_id($post_id) );
+		$featureimageALT = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true );
+		
+		$excerpt .= '<a class="various" href="#post_'.$post->ID.'" title="' .get_the_title(). '" aria-label="Click to see the ' .get_the_title(). ' research area associated faculty"><div class="researchthumbnail" style="background-image: url(' .$featimageURL. ');"><h2>' .get_the_title(). '</h2></div></a>';
+
+		return $excerpt;
+	}
+}
+
+$ResearchAreas = new ResearchAreas();
