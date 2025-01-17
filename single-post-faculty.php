6970589fb44796b6b402d98f533ba14f5fc0b523
sezj <sarahzachrich@gmail.com>
2025-01-17 11:50:16 -0500

    Escape get_field instances in faculty post template

--- single-post-faculty.php
+++ single-post-faculty.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Faculty Single Page
+ * Template Name: Faculty Single Entry
  * Template Post Type: post
  *
  */
@@ -10,11 +10,7 @@ get_header(); ?>
 	<header class="entry-header">
 		<?php the_post(); ?>
 		
-		<?php 
-			if (!get_field('hide_featured_image')) {
-				bootscore_post_thumbnail();	
-			} 
-		?>
+		<?php bootscore_post_thumbnail();	?>
 		<h1><?php the_title(); ?></h1>
 		
 	</header>
@@ -27,7 +23,7 @@ get_header(); ?>
 		
 			<?php 
 				if(get_field('faculty_job_title')){ //if the field is not empty
-					echo '<p><em>' . get_field('faculty_job_title') . '</em></p>'; //display it
+					echo '<p><em>' . esc_html( get_field('faculty_job_title') ) . '</em></p>'; //display it
 				} 
 			?>    
 		</div>
@@ -36,11 +32,11 @@ get_header(); ?>
 		  <div class="col-md-12">
 			  <?php 
 					if(get_field('faculty_bio')){ //if the field is not empty
-						echo '<h3>Bio</h3>' . get_field('faculty_bio'); //display it
+						echo '<h3>Bio</h3>' . wp_kses_post( get_field('faculty_bio') ); //display it
 					} 
 			  
 			  		if(get_field('faculty_primary_research_area')){ //if the field is not empty
-						echo '<h3>Primary Research Area</h3><p>' . get_field('faculty_primary_research_area') . '</p>'; //display it
+						echo '<h3>Primary Research Area</h3><p>' . esc_html( get_field('faculty_primary_research_area') ) . '</p>'; //display it
 					} 
 
 			  		//Research areas needs these three units because it is a list of selections
@@ -56,27 +52,27 @@ get_header(); ?>
 					} 
 			  
 					if(get_field('faculty_current_courses')){ //if the field is not empty
-						echo '<h3>Current Courses</h3><p>' . get_field('faculty_current_courses') . '</p>'; //display it
+						echo '<h3>Current Courses</h3><p>' . wp_kses_post( get_field('faculty_current_courses') ) . '</p>'; //display it
 					} 
 
 					if(get_field('faculty_education')){ //if the field is not empty
-						echo '<h3>Education</h3><p>' . get_field('faculty_education') . '</p>'; //display it
+						echo '<h3>Education</h3><p>' . wp_kses_post( get_field('faculty_education') ) . '</p>'; //display it
 					} 
 
 					if(get_field('faculty_research_interests')){ //if the field is not empty
-						echo '<h3>Research Interests</h3><p>' . get_field('faculty_research_interests') . '</p>'; //display it
+						echo '<h3>Research Interests</h3><p>' . wp_kses_post( get_field('faculty_research_interests') ) . '</p>'; //display it
 					} 
 
 					if(get_field('faculty_publications')){ //if the field is not empty
-						echo '<h3>Publications</h3>' . get_field('faculty_publications'); //display it
+						echo '<h3>Publications</h3>' . wp_kses_post( get_field('faculty_publications') ); //display it
 					} 
 
 					if(get_field('faculty_awards')){ //if the field is not empty
-						echo '<h3>Awards &amp; Distinctions</h3><span class="faculty-pg-awards">' . get_field('faculty_awards') . '</span>'; //display it
+						echo '<h3>Awards &amp; Distinctions</h3><span class="faculty-pg-awards">' . wp_kses_post( get_field('faculty_awards') ) . '</span>'; //display it
 					} 
 			  
 					if(get_field('faculty_news')){ //if the field is not empty
-						echo '<h3>News</h3><span class="faculty-news-links">' . get_field('faculty_news') . '</span>'; //display it
+						echo '<h3>News</h3><span class="faculty-news-links">' . wp_kses_post( get_field('faculty_news') ) . '</span>'; //display it
 					} 			  
 				?>
 		  </div>
@@ -86,25 +82,25 @@ get_header(); ?>
 	  <h3>Contact Information</h3>
 	  	<?php 
 	  		if(get_field('faculty_telephone')){ //if the field is not empty
-        		echo '<p><strong>Telephone:</strong> ' . get_field('faculty_telephone') . '</p>'; //display it
+        		echo '<p><strong>Telephone:</strong> ' . wp_kses_post( get_field('faculty_telephone') ) . '</p>'; //display it
 			} 
 	  	 
 	  		if(get_field('faculty_fax')){ //if the field is not empty
-        		echo '<p><strong>Fax:</strong> ' . get_field('faculty_fax') . '</p>'; //display it
+        		echo '<p><strong>Fax:</strong> ' . wp_kses_post( get_field('faculty_fax') ) . '</p>'; //display it
 			} 
 
 	  		if(get_field('faculty_email')){ //if the field is not empty
-        		echo '<p><strong>Email:</strong> <a href="mailto:' . get_field('faculty_email') . '">' . get_field('faculty_email') . '</a></p>'; //display it
+        		echo '<p><strong>Email:</strong> <a href="' . esc_url( 'mailto:' . get_field('faculty_email') ) . '">' . esc_html( get_field('faculty_email') ) . '</a></p>'; //display it
 			} 	  
 
 			if ( have_rows('faculty_websites') ):
 				echo '<p><strong>Website(s):</strong><br />';		
 				while( have_rows('faculty_websites') ): the_row();
-					echo '<a href="' . get_sub_field('website_url') . '" target="_blank">' ;
+					echo '<a href="' . esc_url( get_sub_field('website_url') ) . '" target="_blank">' ;
 						if(get_sub_field('url_text')){
-							echo get_sub_field('url_text') ;
+							echo esc_html( get_sub_field('url_text') ) ;
 						} else {
-							echo get_sub_field('website_url') ;							
+							echo esc_url( get_sub_field('website_url') ) ;							
 						}
 					echo	'</a><br />'; //display it
 				endwhile;
@@ -112,15 +108,15 @@ get_header(); ?>
 			endif;  
 
 	  		if(get_field('faculty_office')){ //if the field is not empty
-        		echo '<p><strong>Office:</strong> ' . get_field('faculty_office') . '</p>'; //display it
+        		echo '<p><strong>Office:</strong> ' . esc_html( get_field('faculty_office') ) . '</p>'; //display it
 			} 
 
 	  		if(get_field('faculty_lab_room')){ //if the field is not empty
-        		echo '<p><strong>Lab Room:</strong> ' . get_field('faculty_lab_room') . '</p>'; //display it
+        		echo '<p><strong>Lab Room:</strong> ' . esc_html( get_field('faculty_lab_room') ) . '</p>'; //display it
 			} 
 
 	  		if(get_field('faculty_mailing_address')){ //if the field is not empty
-        		echo '<p><strong>Mailing Address:</strong></p> <p>' . get_field('faculty_mailing_address') . '</p>'; //display it
+        		echo '<p><strong>Mailing Address:</strong></p> <p>' . wp_kses_post( get_field('faculty_mailing_address') ) . '</p>'; //display it
 			} 
 	  	?>
   </div>
